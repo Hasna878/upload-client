5094582b7d2e6f6891cc5c5d8ea38d70f07cb1bc
Hasna Boubakry <mejustme9275@gmail.com>
2025-12-06 18:20:02 +0100

    Add basic JUnit test for UploadClient

---
+++ test/java/com/iot/project/UploadClientTest.java
@@ -0,0 +1,13 @@
+package com.iot.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UploadClientTest {
+
+    @Test
+    void testAppStarts() {
+        // Simple positive assertion
+        assertTrue(true, "Basic test to validate JUnit setup");
+    }
+}
